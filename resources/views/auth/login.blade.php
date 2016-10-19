fb2e003854a5868f020e9106e62090f67899e0ea
Dominik Dornacher <dornacher.d@outlook.com>
2016-10-19 14:15:38 -0500

    new

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app2')
+
+@section('content')
+    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+        {{ csrf_field() }}
+
+        <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
+            <label for="email" class="col-md-4 control-label"></label>
+
+            <div class="col-md-4">
+                <input id="email" type="email" class="form-control" name="email" placeholder="Email Adress" value="{{ old('email') }}" required autofocus>
+
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group {{ $errors->has('password') ? ' has-error' : '' }}">
+            <label for="password" class="col-md-4 control-label"></label>
+
+            <div class="col-md-4">
+                <input id="password" type="password" class="form-control" placeholder="Password" name="password" required>
+
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-md-6 col-md-offset-2">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="remember">Remember me
+                    </label>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-md-7 col-md-offset-2">
+                <button type="submit" class="btn btn-primary">
+                    Login
+                </button>
+
+                <a class="btn btn-link" href="{{ url('/password/reset') }}">
+                    Forgot Your Password?
+                </a>
+            </div>
+        </div>
+    </form>
+    
+@endsection
