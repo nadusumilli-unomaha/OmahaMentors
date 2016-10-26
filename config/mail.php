0bf2377b6db0099798fafee8cf4e66e55fcdaad3
nadusumilli <nadusumilli@unomaha.edu>
2016-10-26 17:42:50 -0500

    Added production values for mail.

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 2525),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,7 +84,7 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('MAIL_USERNAME', 'superawesome@nikhitadusumilli.me'),
 
     /*
     |--------------------------------------------------------------------------
@@ -97,7 +97,7 @@ return [
     |
     */
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('MAIL_PASSWORD', 'Tufluckfed1'),
 
     /*
     |--------------------------------------------------------------------------
